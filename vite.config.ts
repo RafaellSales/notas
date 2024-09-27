d33528093ec12d1da4462e5f196617cdc995555d
RafaellSales <rafael.si2018.2@gmail.com>
2024-09-26 21:36:20 -0300

    URL absoluta

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: "./src",
+  base: "./",
   plugins: [react()],
   build: {
     rollupOptions: {
